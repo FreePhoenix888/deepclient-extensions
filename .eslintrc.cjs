d3df92f8250ef4185f941668a6086c0f1b572048
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-04 18:49:52 +0600

    Prettier write

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
-    "prettier"
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
