558f5cbb718159ba7d52f0fd1593754be7313ab0
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-04 19:06:07 +0600

    Add exports and sort them

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,52 @@
-// export {} from './package-installer-decorator'
+export {
+  PackageInstallerDecorator,
+  createPackageInstallerDecorator,
+} from "./package-installer-decorator/create-package-installer-decorator.js";
+export {
+  InstallPackagesOptions,
+  installPackages,
+} from "./package-installer-decorator/install-packages.js";
 export {
   makeInstallPackagesOperations,
   type MakeInstallPackagesOperationsOptions,
   type InstallPackagesOperationsPerPackageName,
   type PackageName,
 } from "./package-installer-decorator/make-install-packages-serial-operations.js";
-
+export {
+  AppendTransitionOptions,
+  append,
+} from "./serial-transitions-builder/append.js";
+export { clear } from "./serial-transitions-builder/clear.js";
+export {
+  CreateTransitionBuilderDecoratorOptions,
+  SerialTransitionsBuilderDecorator,
+  createSerialTransitionsBuilderDecorator,
+} from "./serial-transitions-builder/create-serial-transitions-builder-decorator.js";
+export {
+  ExecuteOptions,
+  execute,
+} from "./serial-transitions-builder/execute.js";
+export { getTransitionType } from "./serial-transitions-builder/get-transition-type.js";
+export {
+  DeleteSerialAction,
+  InsertSerialAction,
+  SerialAction,
+  UpdateSerialAction,
+} from "./serial-transitions-builder/serial-action.js";
+export {
+  Transition,
+  TransitionDeleteType,
+  TransitionInsertType,
+  TransitionItem,
+  TransitionType,
+  TransitionUpdateType,
+} from "./serial-transitions-builder/transition.js";
+export {
+  insertClientHandler,
+  type InsertClientHandlerParam,
+} from "./insert-client-handler";
 export { insertHandler, type InsertHandlerParam } from "./insert-handler";
 export {
   insertRouteHandler,
   type InsertRouteHandlerParam,
 } from "./insert-route-handler";
-export {
-  insertClientHandler,
-  type InsertClientHandlerParam,
-} from "./insert-client-handler";
